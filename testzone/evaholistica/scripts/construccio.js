eeb76d7d3bdfd356df30b2215a79b0de69762d7c
EVAMUNCU <141054085+EVAMUNCU@users.noreply.github.com>
2023-09-04 18:41:36 +0200

    Add files via upload

---
+++ testzone/evaholistica/scripts/construccio.js
@@ -0,0 +1,3 @@
+window.onload = function()  {
+	alert("Esta web está en construcción.\nTen en cuenta que muchos elementos no se encuentran disponibles o no funcionan.\nTambién se pueden experimentar problemas con la visualización del contenido en algunas pantallas de ciertos móviles. Disculpad las molestias.")
+}
